7f6a6b3fb1ff51acabc5ae8052449b48fa53a02a
Tunahan Turgut <114873393+Bizbize60@users.noreply.github.com>
2023-01-21 01:11:29 +0300

    en yenisi

--- mavi.py
+++ mavi.py
@@ -12,7 +12,7 @@ class payments_debts:
         self.company_name=company_name
         self.debt=input("Borç?")
         self.payment=input("ALINACAK?")
-        sql="INSERT INTO dokumanlar (sirket, borc , gelecek) VALUES (%s, %s ,%s)"
+        sql="INSERT INTO MAVI (sirket, borc , alinacak) VALUES (%s, %s ,%s)"
         val=(self.company_name,self.debt,self.payment)
         mycursor.execute(sql,val)
         mydb.commit()
@@ -20,27 +20,82 @@ class payments_debts:
     def __str__(self):
         return f"Şirket Adı:{self.company_name} Tarih:{self.date} Borç:{self.debt} Alınacak:{self.payment}"
 
-def islemler():   
+def islemler():
     while(True):
-        print("1:Yeni Bir Döküman Girişi \n2:Borçlar Alınacaklar \n ")
+        print("1:Yeni Bir Döküman Girişi \n2:TÜM GEÇMİŞ\n3:TEK BİR ŞİRKETLE OLAN GEÇMİŞ\n4:ZATEN VAR OLAN ŞİRKET\n ")
         choice=input("Hangi İşlemi Seçmek İstersiniz?")
         if choice=="1":
-            print("Çıkmak İçin  0")
-            print("ŞİRKET İSMİ")
-            company_name=input("")
-            if company_name=="0":
-                continue
-                
-            company_name=payments_debts(company_name)
+            add_new_document()
         if choice=="2":
-            mycursor.execute("SELECT * FROM dokumanlar")
-            result=mycursor.fetchall()
-            for results in result:
-                print(results)
+            list_all()
+        if choice=="3":
+            specific_company()
+        if choice=="4":
+            add_to_document()
+def add_new_document():
+    print("Çıkmak İçin  0")
+    print("ŞİRKET İSMİ")
+    company=input("")
+    if company=="0":
+        islemler()
+    sql=(f"SELECT * FROM MAVI WHERE sirket='{company}'")
+    mycursor.execute(sql)
+    if mycursor.fetchall():
+        print("BÖYLE BİR KAYIT ZATEN VAR")
+        islemler()
+    company=payments_debts(company)            
+def list_all():
+    mycursor.execute("SELECT * FROM mavi")
+    result=mycursor.fetchall()
+    for results in result:
+        print(results)
+def specific_company():   
+    company=input("HANGİ ŞİRKET?")
+    sql=(f"SELECT * FROM MAVI WHERE sirket='{company}'")
+    mycursor.execute(sql)
+    if not mycursor.fetchall():
+        print("BÖYLE BİR ŞİRKET YOK")
+        islemler()
+        results=mycursor.fetchall()
+        i=0
+    for data in results:
+        print(data)
+        i+=1
+        if i==5:
+            break
+    sql=(f"SELECT borc FROM MAVI WHERE sirket='{company}'")
+    mycursor.execute(sql)
+    results=mycursor.fetchall()
+    total_borc=0
+    for borc in list(results):
+        liste=list(borc)
+        total_borc+=liste[0]
 
-        
+    sql=(f"SELECT alinacak FROM MAVI WHERE sirket='{company}'")
+    mycursor.execute(sql)
+    results=mycursor.fetchall()
+    total_alinacak=0
+    for alinacak in results:
+        liste=list(alinacak)
+        total_alinacak+=liste[0]
+    if total_alinacak>total_borc:
+        print("+",total_alinacak-total_borc)
+    else:
+        print("-",total_borc-total_alinacak)  
+def add_to_document():
+    company=input("HANGİ ŞİRKET?")
+    sql=f"(SELECT * FROM mavi WHERE sirket='{company}')"
+    mycursor.execute(sql)
 
-
+    if not mycursor.fetchall():
+        print("BÖYLE BİR KAYIT YOK")
+    else:
+        borc=input("borc?")
+        alinacak=input("alinacak?")
+        sql="INSERT INTO MAVI (sirket,borc,alinacak) VALUES (%s, %s, %s)"
+        val=(company,borc,alinacak)
+        mycursor.execute(sql, val)
+        mydb.commit()        
                         
 mydb = mysql.connector.connect(
     host="localhost",
@@ -50,10 +105,10 @@ mydb = mysql.connector.connect(
 )
 mycursor=mydb.cursor()
 mycursor.execute("SHOW TABLES")
-if ('dokumanlar',) in mycursor:
+if ('mavi',) in mycursor:
     islemler()
 else:
-    mycursor.execute("CREATE TABLE dokumanlar (sirket VARCHAT(255),borc INT,alinacak INT)")
+    mycursor.execute("CREATE TABLE MAVI (sirket VARCHAR(255),borc INT,alinacak INT,tarih DATE)")
     islemler()
 
 
